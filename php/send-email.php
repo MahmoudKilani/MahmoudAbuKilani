dff01ae9ede65a9252b60c51997b434aa4b845b1
Mahmoud Ahmed Kilani <77079544+MahmoudKilani@users.noreply.github.com>
2023-05-04 19:20:28 +0300

    Update send-email.php

--- php/send-email.php
+++ php/send-email.php
@@ -1,27 +1,12 @@
-<?php
-require_once('https://mahmoudkilani.000webhostapp.com/php/send-email.php');
 if(isset($_POST['submit'])){
-    $to = "mahmoudahmedkilani@gmail.com";
-    $subject = "New message from your website";
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    // Get IP address of the user
-    $ip = $_SERVER['REMOTE_ADDR'];
     
-    // Make a request to a geolocation API to get the location data
-    $url = "http://ip-api.com/json/$ip";
-    $data = file_get_contents($url);
-    $location = json_decode($data);
+    // Call the external PHP file to send the email
+    $result = include('http://example.com/send-email.php?name=' . $name . '&email=' . $email . '&message=' . $message);
     
-    // Format the location data into a string
-    $location_str = $location->city . ', ' . $location->regionName . ', ' . $location->country;
-    
-    // Add the location to the message
-    $message .= "\n\nLocation: $location_str";
-    
-    $headers = "From: " . $name . " <" . $email . ">\r\n";
-    if(mail($to, $subject, $message, $headers)){
+    if($result){
         // Show success message popup
         echo '<div class="popup success">Thank you for contacting us, we will get back to you soon!</div>';
     } else {
@@ -29,5 +14,3 @@ if(isset($_POST['submit'])){
         echo '<div class="popup error">Sorry, an error occurred. Please try again later.</div>';
     }
 }
-?>
-https://mahmoudkilani.000webhostapp.com/php/send-email.php
