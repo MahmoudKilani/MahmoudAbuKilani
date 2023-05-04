c95e1d6502c7c35095df6ec26e27be5ee11a39b1
Mahmoud Ahmed Kilani <77079544+MahmoudKilani@users.noreply.github.com>
2023-05-04 18:34:46 +0300

    Update send-email.php

--- php/send-email.php
+++ php/send-email.php
@@ -1,6 +1,10 @@
 <?php
 if(isset($_POST['submit'])){
-    require_once "vendor/autoload.php"; // Path to the Composer autoloader
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require 'path/to/PHPMailer/src/PHPMailer.php';
+    require 'path/to/PHPMailer/src/Exception.php';
+    require 'path/to/PHPMailer/src/SMTP.php';
 
     $to = "mahmoudahmedkilani@gmail.com"; // Recipient email address
     $subject = "New message from your website";
@@ -8,27 +12,34 @@ if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    // Configure the SMTP connection
-    $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
-        ->setUsername('mahmoudahmedkilani@gmail.com') // Your Gmail email address
-        ->setPassword('7A\iRT&=czd*&*0[x]Io<?Vx1$$'); // Your Gmail password
+    // Create the PHPMailer object
+    $mail = new PHPMailer(true);
 
-    // Create the Mailer using the SMTP connection
-    $mailer = new Swift_Mailer($transport);
+    try {
+        // Configure the SMTP connection
+        $mail->isSMTP();                                            
+        $mail->Host       = 'smtp.gmail.com';                     
+        $mail->SMTPAuth   = true;                                   
+        $mail->Username   = 'mahmoudahmedkilani@gmail.com';                     
+        $mail->Password   = '7A\iRT&=czd*&*0[x]Io<?Vx1$$';                               
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;           
+        $mail->Port       = 465;                                    
 
-    // Create the message
-    $message = (new Swift_Message($subject))
-        ->setFrom([$email => $name])
-        ->setTo([$to])
-        ->setBody($message);
+        // Set the message properties
+        $mail->setFrom($email, $name);
+        $mail->addAddress($to);
+        $mail->isHTML(false);
+        $mail->Subject = $subject;
+        $mail->Body    = $message;
 
-    // Send the message
-    if($mailer->send($message)){
+        // Send the message
+        $mail->send();
         // Show success message popup
         echo '<div class="popup success">Thank you for contacting us, we will get back to you soon!</div>';
-    } else {
+    } catch (Exception $e) {
         // Show error message popup
         echo '<div class="popup error">Sorry, an error occurred. Please try again later.</div>';
     }
 }
 ?>
+
