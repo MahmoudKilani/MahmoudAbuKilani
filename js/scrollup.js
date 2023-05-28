6ce4a3259326b7de5a31bd5e2b326df6473bd4ab
fack name <77079544+MahmoudKilani@users.noreply.github.com>
2023-05-28 18:57:15 +0300

    Add files via upload

--- js/scrollup.js
+++ js/scrollup.js
@@ -1,20 +1,17 @@
-  // Scroll to top button show/hide
-  window.addEventListener('scroll', function() {
-      var scrollUp = document.getElementById('scroll-up');
-      if (window.scrollY > 200) {
-          scrollUp.classList.add('show');
-      } else {
-          scrollUp.classList.remove('show');
-      }
-  });
+// Scroll to top button show/hide
+$(window).scroll(function() {
+    var scrollUp = $('#scroll-up');
+    if ($(this).scrollTop() > 200) {
+        scrollUp.addClass('show');
+    } else {
+        scrollUp.removeClass('show');
+    }
+});
 
-  // Smooth scrolling using jQuery easing
-  $('a.scrollup').on('click', function(event) {
-      var target = $(this.getAttribute('href'));
-      if (target.length) {
-          event.preventDefault();
-          $('html, body').stop().animate({
-              scrollTop: target.offset().top
-          }, 1000);
-      }
-  });
+// Smooth scrolling using jQuery easing
+$('a.scrollup').on('click', function(event) {
+    event.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: 0
+    }, 1000);
+});
