8bcaf96b45bcac51be0376f34676f6611873746f
fack name <77079544+MahmoudKilani@users.noreply.github.com>
2023-05-28 05:24:30 +0300

    Update sendmailPopup.js

--- js/sendmailPopup.js
+++ js/sendmailPopup.js
@@ -2,6 +2,50 @@ document.querySelector('.close-icon').addEventListener('click', function() {
     document.getElementById('email-popup').style.display = 'none';
 });
 
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    // Perform form validation
+    if (name === '' || email === '' || message === '') {
+        showPopup('Please fill in all fields.', false);
+        return false;
+    }
+
+    // Send the email using AJAX
+    var xhr = new XMLHttpRequest();
+    var url = 'send-email.php'; // Replace with the correct URL to your PHP file
+    var params = 'name=' + encodeURIComponent(name) + '&email=' + encodeURIComponent(email) + '&message=' + encodeURIComponent(message);
+
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var response = xhr.responseText;
+                // Display success or error message based on the response
+                showPopup(response, response === 'success');
+            } else {
+                // Show error message if there was an issue with the AJAX request
+                showPopup('Sorry, an error occurred. Please try again later.', false);
+            }
+        }
+    };
+
+    xhr.send(params);
+
+    // Reset form fields
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+
+    return false;
+});
+
 function showPopup(message, success) {
     var popup = document.getElementById('email-popup');
     var popupIcon = popup.querySelector('.popup-icon');
