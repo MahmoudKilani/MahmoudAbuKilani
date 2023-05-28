bafbad153d73eb42a403df5d9ac42655646751b1
fack name <77079544+MahmoudKilani@users.noreply.github.com>
2023-05-28 07:04:18 +0300

    Update sendmailPopup.js

--- js/sendmailPopup.js
+++ js/sendmailPopup.js
@@ -24,19 +24,21 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
     xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            console.log(response); // Log the response to check the values
-            // Display success or error message based on the response
-            showPopup(response.message, response.status === 'success');
-        } else {
-            // Show error message if there was an issue with the AJAX request
-            showPopup('Sorry, an error occurred. Please try again later.', false);
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                // Display success or error message based on the response
+                if (response.status === 'success') {
+                    showPopup('Message sent successfully.', true);
+                } else {
+                    showPopup('Error sending message. Please try again later.', false);
+                }
+            } else {
+                // Show error message if there was an issue with the AJAX request
+                showPopup('Sorry, an error occurred. Please try again later.', false);
+            }
         }
-    }
-};
-
+    };
 
     xhr.send(params);
 
